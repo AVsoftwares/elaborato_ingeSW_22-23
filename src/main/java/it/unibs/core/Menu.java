be75a8e93467014372f8039ca527d9a732076fd5
Marco Galli <marco.galli@proton.me>
2023-04-02 23:18:19 +0200

    Initialize Maven project

--- src/main/java/it/unibs/core/Menu.java
+++ src/main/java/it/unibs/core/Menu.java
@@ -1,4 +1,4 @@
-package it.unibs;
+package it.unibs.core;
 
 public abstract class Menu {
 
--- src/main/java/it/unibs/core/Restaurant.java
+++ src/main/java/it/unibs/core/Restaurant.java
@@ -1,11 +1,14 @@
-package it.unibs;
+package it.unibs.core;
 
+
+import java.io.File;
+import java.util.List;
 
 public class Restaurant {
-    private int coversNumber=0;
-    private int avgBeverageEach=0;
-    private int avgExtraEach=0;
-    private int individualWorkLoad=0;
+    private int coversNumber = 0;
+    private int avgBeverageEach = 0;
+    private int avgExtraEach = 0;
+    private int individualWorkLoad = 0;
 
     private String restaurantName;
 
@@ -13,26 +16,28 @@ public class Restaurant {
         this.restaurantName = restaurantName;
     }
 
-    public void initializeDataConfig(File f){
+    public void initializeDataConfig(File f) {
         //Gestore inizializza da file i parametri avgBeverageEach, coversNumber, avgExtraEach..
 
     }
+
     //magari il metodo sopra e questo sotto lo mettiamo in una classe di utility tipo RestaurantUtility
-    private void setRestaurantMealWorkLoad(int IndividualWorkLoad, int coversNumber){
+    private void setRestaurantMealWorkLoad(int IndividualWorkLoad, int coversNumber) {
         //prodotto del
         //carico di lavoro per persona per il numero complessivo di posti a sedere del ristorante
         //accresciuto del 20%.
     }
-    public void showDataConfig(){
+
+    public void showDataConfig() {
 
     }
 
     //questi due metodi forse meglio spostarli sul Gestore?
-    public void makeMenu(List recipe){
+    public void makeMenu(List recipe) {
 
     }
 
-    public void showMenus(){
+    public void showMenus() {
 
     }
 
