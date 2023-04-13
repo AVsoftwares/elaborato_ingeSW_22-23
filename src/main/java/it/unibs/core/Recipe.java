7aed8a1991d9d1dabec1e7fb818ccbcfcac723ff
AVsoftwares <avdesignsoftware@gmail.com>
2023-04-13 18:08:38 +0200

    aggiunta libreria lombok e scheletro crea ricetta

--- src/main/java/it/unibs/core/Recipe.java
+++ src/main/java/it/unibs/core/Recipe.java
@@ -1,12 +1,18 @@
 package it.unibs.core;
 
-import java.util.Map;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Map;
+import java.util.Scanner;
+
+@Data
+@NoArgsConstructor
 public class Recipe {
     private String recipeName;
     private int portions;
     private int portionWorkLoad;
-    private int personWokload;
+    private Float personWokload;
     private int preparationTime;
     private Map<Ingredient, Float> ingredientAmount;
 
@@ -57,6 +63,34 @@ public class Recipe {
 
     public String getName(){
         return this.recipeName;
+    }
+
+    public void createNewRecipe(){
+
+        Recipe recipe= new Recipe();
+        Scanner scanner = new Scanner(System.in);
+        String inputIngredient = "";
+        Integer ingredientCount=0;
+        System.out.println("Ciao, Per inserire una nuova ricetta scegli il nome: ");
+        recipe.setRecipeName(scanner.nextLine());
+        while (!inputIngredient.equalsIgnoreCase("N")) {
+            ingredientCount++;
+            System.out.printf("Inserisci l'ingrediente %d: ",ingredientCount);
+            addIngredientToRecipe(recipe);
+            System.out.println("Vuoi inserire un altro ingrediente (Y/N)?: ");
+            inputIngredient = scanner.nextLine();
+
+        }
+        System.out.println("Quante porzioni puoi produrre con le dosi che hai inserito?: ");
+        recipe.setPortions(scanner.nextInt());
+        System.out.println("Qual'è il carico di lavoro per porzione di questa ricetta (numero decimale minore di uno)?:  ");
+        recipe.setPersonWokload(scanner.nextFloat());
+
+        System.out.printf("La ricetta %s è stata inserita nel ricettario",recipe.getRecipeName());
+    }
+
+    public void addIngredientToRecipe (Recipe recipe){
+
     }
 
 }
