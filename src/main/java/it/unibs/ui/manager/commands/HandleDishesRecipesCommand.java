e7b08921824d4b4b9e3dc28ffa6c7d5a2fa5fd1e
Marco Galli <marco.galli@proton.me>
2023-04-26 16:09:24 +0200

    Modifica comando

--- src/main/java/it/unibs/ui/manager/commands/HandleDishesRecipesCommand.java
+++ src/main/java/it/unibs/ui/manager/commands/HandleDishesRecipesCommand.java
@@ -3,6 +3,9 @@ package it.unibs.ui.manager.commands;
 import it.unibs.core.Restaurant;
 import it.unibs.ui.Command;
 
+/**
+ * Gestisce le corrispondenze tra piatti e ricette
+ */
 public class HandleDishesRecipesCommand implements Command {
 
     private final Restaurant restaurant;
